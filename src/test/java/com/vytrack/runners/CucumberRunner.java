e2ea165bbf9e1a379e4dab44c0b09d66327768a2
git config --global user.name <yonashaile12@gmail.com>
2020-11-08 09:03:19 -0600

    Selenium grid

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/vytrack/step_definitions",
         dryRun = false,
-        tags = " @add_car_scenario_outline",
+        tags = "@add_car",
         publish = true
 )
 public class CucumberRunner {
--- src/test/java/com/vytrack/step_definitions/Hooks.java
+++ src/test/java/com/vytrack/step_definitions/Hooks.java
@@ -4,58 +4,60 @@ import com.vytrack.utils.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 import java.util.concurrent.TimeUnit;
 
 /**
- * Hooks name is not reserved. you may name this class in any way.
- * For example: SuitSetUpAndTestDown.
+ * Hooks name is not reserved. You may name this class in any way.
+ * For example: SuiteSetupAndTearDown
  * Hooks triggered based on tags not class name or their location.
- * These methods can be part of any step definition class.
+ * These methods can be a part of any step definition class.
  * Common practice is to store them in the separate class.
  */
-
-
 public class Hooks {
 
-//   hook before == @BeforeMethod in TestNG
-//   Hook after == @AfterMethod in TestNG
-//   it's not a good idea to mix implicit and explicit waits. It can lead to unexpectedly long waits.
-//   usually, we just use explicit and fluent waits
+//    hook before = @BeforeMethod in TestNG
+//    hook after = @AfterMethod in TestNG
+//    it's not a good idea to mix implicit and explicit waits. It can lead to unexpectedly long waits.
+//    usually, we just use explicit and fluent waits.
 
     @Before
-    public void setup(Scenario scenario){
-        System.out.println(scenario.getSourceTagNames());
-        System.out.println("::: Starting Automation :::");
+    public void setup(Scenario scenario) {
+        System.out.println("::: Starting Automation:::");
         Driver.getDriver().manage().window().maximize();
         Driver.getDriver().manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
     }
-
-    //this hook will run only before scenarios with a tag @db
+//    this hook will run only before scenarios with a tag @db
 
     /**
-     * Scenario: I don't know but here I'm connecting to DB
-     * Given nuser runs following query "SELECT *..."
-     *
-     * order = 0-to define hooks
+     * @db Scenario: I don't know but here I'm connecting to DB
+     * Given user runs following query "SELECT * ...."
+     * <p>
+     * order = 0 - to define hooks execution order
      */
     @Before(value = "@db", order = 0)
-    public void dbSetup(){
-        System.out.println("::: Connecting to database :::");
+    public void dbSetup() {
+        System.out.println("::: Connecting to the database:::");
     }
 
-    @After("db")
-    public void dbTearDown(){
-        System.out.println("::: Disconnecting from the database :::");
+    @After("@db")
+    public void dbTearDown() {
+        System.out.println("::: Disconnecting from the database:::");
     }
 
     @After
-    public void teardown(){
-        //close browser, close DB connection, close tunnel, take a screen shot
-        // this is a hook after
-        // runs automatically after every test
-        Driver.closeDriver();
-        System.out.println(":::(^_^) End of test execution (*_*) :::");
-    }
+    public void tearDown(Scenario scenario) {
+        //close browser, close DB connection, close tunnel,capture screenshot of the error, etc..
+        //this is a hook after
+        //runs automatically after every test
+        if (scenario.isFailed()){
+            byte[] data = ((TakesScreenshot)Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(data, "image/png", scenario.getName());
+        }
 
+        Driver.closeDriver();
+        System.out.println(":::(^_^) End of test execution (*_*):::");
+    }
 }
